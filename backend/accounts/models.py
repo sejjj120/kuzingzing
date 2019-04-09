85294a296412c2e29796b438e26acfcb3b679a7d
김세진 <sejjj120@gmail.com>
2019-04-09 23:19:04 +0900

    User model field 추가 진행중

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    age = models.DecimalField(max_digits=3, decimal_places=0)
+    gender = (
+        ('남', 'Male'),
+        ('여', 'Female'),
+    )
