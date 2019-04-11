6b33521265afc32dc7adf2aed7afc64718cbbe5b
김세진 <sejjj120@gmail.com>
2019-04-11 15:10:04 +0900

    urlpattern 변경

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,3 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views as user_views
-
-urlpatterns = [
-    path('login/', include(auth_views.LoginView)),
-    path('logout/', include(auth_views.LogoutView))
-]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('accounts/', include('accounts.urls')),
+    path(r'^accounts/', include('django.contrib.auth.urls')),
 ]
