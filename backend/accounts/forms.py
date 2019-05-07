4e5a168b8e0fe1fc406064d71feabea8744969bf
김세진 <sejjj120@gmail.com>
2019-05-07 13:36:49 +0900

    erase for using knox auth

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -1,8 +1,8 @@
-from django import forms
-# from .models import User
-from django.contrib.auth.models import User
+# from django import forms
+# # from .models import User
+# from django.contrib.auth.models import User
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
+# class UserForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ['username', 'password']
