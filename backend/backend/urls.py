56685dbb1a2ddd4abac3ac9e3661d0a280bcd650
김세진 <sejjj120@gmail.com>
2019-05-07 13:46:02 +0900

    knox auth

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
     path('', include('main.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('register', user_views.register),
 ]
