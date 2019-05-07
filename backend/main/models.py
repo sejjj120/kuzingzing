6e4909f62e82b57e2c9a0c426df97c4814b4549b
doontagi <suamzzz@korea.ac.kr>
2019-05-07 19:38:56 +0900

    model, serializer modified

--- backend/main/models.py
+++ backend/main/models.py
@@ -6,12 +6,12 @@ class Agenda(models.Model):
     area = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     timestamp = models.DateTimeField(auto_now_add=True)
-    creator = models.ForeignKey(User, related_name='agendas', on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, related_name='agendas', on_delete=models.CASCADE, null=True)
 
 
 class Like(models.Model):
     agenda = models.ForeignKey(Agenda, related_name='agenda_likes', on_delete=models.CASCADE)
-    creator = models.ForeignKey(User, related_name='user_likes', on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, related_name='user_likes', on_delete=models.CASCADE, null=True)
     likenum = models.IntegerField()
     dislikenum = models.IntegerField()
 
@@ -19,6 +19,6 @@ class Like(models.Model):
 class Comment(models.Model):
     agenda = models.ForeignKey(Agenda, related_name='agenda_comments', on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
-    creator = models.ForeignKey(User, related_name='user_comments', on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, related_name='user_comments', on_delete=models.CASCADE, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     like = models.IntegerField()
--- backend/main/serializers.py
+++ backend/main/serializers.py
@@ -25,12 +25,13 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class AgendaSerializer(serializers.ModelSerializer):
-    agenda_comments = CommentSerializer(many=True)
-    agenda_likes = LikeSerializer(many=True)
-    total_likes = serializers.IntegerField()
-    total_dislikes = serializers.IntegerField()
+    agenda_comments = CommentSerializer(many=True, required=False, read_only=True)
+    agenda_likes = LikeSerializer(many=True,required=False, read_only=True)
+    total_likes = serializers.IntegerField(required=False, read_only=True)
+    total_dislikes = serializers.IntegerField(required=False, read_only=True)
 
     class Meta:
         model = Agenda
         fields = ('area', 'description', 'timestamp',
                   'creator', 'agenda_comments', 'agenda_likes', 'total_likes', 'total_dislikes')
+
