adf60deff5a23a7c82034ca93c5f6e3f4813a8a1
김세진 <sejjj120@gmail.com>
2019-05-07 13:52:57 +0900

    erase MyUser

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -6,7 +6,6 @@ router = DefaultRouter()
 router.register(r'Agendas', views.AgendaViewSet)
 router.register(r'Comments', views.CommentViewSet)
 router.register(r'Likes', views.LikeViewSet)
-router.register(r'Users', views.UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
