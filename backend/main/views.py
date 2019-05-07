b7eede024a704c898e65c8d1261174e9cbd2886d
doontagi <suamzzz@korea.ac.kr>
2019-05-07 19:32:41 +0900

    permission

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,4 +1,6 @@
 from main.models import Agenda, Comment, Like
+from rest_framework import permissions
+from main.permission import IsOwnerOrReadOnly
 from rest_framework import viewsets
 from django.db.models import Sum, Count
 from main.serializers import AgendaSerializer, CommentSerializer, LikeSerializer
@@ -12,19 +14,31 @@ from main.serializers import AgendaSerializer, CommentSerializer, LikeSerializer
 class AgendaViewSet(viewsets.ModelViewSet):
     queryset = Agenda.objects.all()
     serializer_class = AgendaSerializer
-
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
     def get_queryset(self):
         return Agenda.objects.annotate(
             total_likes=Sum('agenda_likes__likenum'),
             total_dislikes=Sum('agenda_likes__dislikenum')
         )
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
 
 
 class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
